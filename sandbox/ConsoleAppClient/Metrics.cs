5058f31e60270c661ee22942883d444732150707
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-02-23 16:53:50 +0900

    Update sample

--- sandbox/ConsoleAppClient/Metrics.cs
+++ sandbox/ConsoleAppClient/Metrics.cs
@@ -6,7 +6,7 @@ namespace SignalStreamingSamples.ConsoleAppClient
         public string Timestamp;
         public uint[] ReceivedMessageCountByClientId = new uint[SampleClient.MAX_CLIENT_COUNT];
         public float[] AveragePayloadSizeByClientId = new float[SampleClient.MAX_CLIENT_COUNT];
-        public int[] MaxPayloadSizeByClientId = new int[SampleClient.MAX_CLIENT_COUNT];
-        public int[] TotalPayloadSizeByClientId = new int[SampleClient.MAX_CLIENT_COUNT];
+        public long[] MaxPayloadSizeByClientId = new long[SampleClient.MAX_CLIENT_COUNT];
+        public long[] TotalPayloadSizeByClientId = new long[SampleClient.MAX_CLIENT_COUNT];
     }
 }
