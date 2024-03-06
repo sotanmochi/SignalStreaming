cbc1ea60e4f9c2eb3b2b8edbf39e670e6b367c0d
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-03-06 10:44:27 +0900

    Update sample

--- src/SignalStreaming.Unity/Packages/SignalStreaming/Samples~/ENetSample/Client/SampleClient.cs
+++ src/SignalStreaming.Unity/Packages/SignalStreaming/Samples~/ENetSample/Client/SampleClient.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Diagnostics;
 using System.Buffers;
+using System.Threading;
 using MessagePack;
 using SignalStreaming.Infrastructure.ENet;
 using UnityEngine;
+using Button = UnityEngine.UI.Button;
+using Text = UnityEngine.UI.Text;
+using Debug = UnityEngine.Debug;
 
 namespace SignalStreaming.Samples.ENetSample
 {
@@ -11,44 +16,103 @@ namespace SignalStreaming.Samples.ENetSample
         [SerializeField] string _serverAddress = "localhost";
         [SerializeField] ushort _port = 3333;
         [SerializeField] string _connectionKey = "SignalStreaming";
+        [SerializeField] string _groupId = "01HP8DMTNKAVNQDWCBMG9NWG8S";
+
+        [SerializeField] PlayerMoveSystem _playerMoveSystem;
+
+        [SerializeField] Button _connectionButton;
+        [SerializeField] Text _connectionButtonText;
+        [SerializeField] Button _transmissionButton;
+        [SerializeField] Text _transmissionButtonText;
+        [SerializeField] Button _resetButton;
+
+        [SerializeField] Text _receivedSignalCountText;
+        [SerializeField] Text _signalsPerSecondText;
+        [SerializeField] Text _receivedSignalCountText1;
+        [SerializeField] Text _signalsPerSecondText1;
+        [SerializeField] Text _receivedSignalCountText2;
+        [SerializeField] Text _signalsPerSecondText2;
+        [SerializeField] Text _receivedSignalCountText3;
+        [SerializeField] Text _signalsPerSecondText3;
+
+        readonly Stopwatch _stopwatch = new();
+
+        CancellationTokenSource _connectionCts;
+        bool _transmissionEnabled;
+
+        long _previousMeasuredTimeMilliseconds;
+        uint _receivedSignalCount;
+        uint _previousMeasuredSignalCount;
+        float _receivedSignalsPerSecond;
+
+        uint _receivedSignalCount1;
+        uint _previousMeasuredSignalCount1;
+        float _receivedSignalsPerSecond1;
+
+        uint _receivedSignalCount2;
+        uint _previousMeasuredSignalCount2;
+        float _receivedSignalsPerSecond2;
+
+        uint _receivedSignalCount3;
+        uint _previousMeasuredSignalCount3;
+        float _receivedSignalsPerSecond3;
 
         ISignalStreamingClient _streamingClient;
         ISignalTransport _transport;
-        IConnectParameters _connectParameters;
+        PlayerMoveController _localPlayerMoveController;
+        uint _clientId;
 
         void Awake()
         {
-            // _transport = new ENetTransport(useAnotherThread: true, targetFrameRate: 60, isBackground: true);
-            _transport = new ENetTransport(useAnotherThread: false, targetFrameRate: 60, isBackground: true);
+            Application.targetFrameRate = 60;
+            _stopwatch.Start();
 
-            _transport.OnConnected += () => Debug.Log($"[{nameof(SampleClient)}] TransportConnected");
-            _transport.OnDisconnected += () => Debug.Log($"[{nameof(SampleClient)}] TransportDisconnected");
-            _transport.OnIncomingSignalDequeued += (payload) => Debug.Log($"[{nameof(SampleClient)}] TransportDataReceived - Payload.Length: {payload.Length}");
-
-            _connectParameters = new ENetConnectParameters()
+            _connectionButtonText.text = "Connect to server";
+            _connectionButton.onClick.AddListener(() =>
             {
-                ConnectionRequestData = System.Text.Encoding.UTF8.GetBytes(_connectionKey),
-                ServerAddress = _serverAddress,
-                ServerPort = _port
-            };
+                if (_connectionCts != null)
+                {
+                    _connectionCts.Cancel();
+                    _connectionCts.Dispose();
+                    _connectionCts = null;
+                    _connectionButtonText.text = "Connect to server";
+                }
+                else
+                {
+                    _connectionCts = new CancellationTokenSource();
+                    ConnectAsync(_connectionCts.Token);
+                    _connectionButtonText.text = "Cancel connection";
+                }
+            });
 
+            _transmissionButtonText.text = "Start transmission";
+            _transmissionButton.onClick.AddListener(() =>
+            {
+                _transmissionEnabled = !_transmissionEnabled;
+                _transmissionButtonText.text = _transmissionEnabled ? "Stop transmission" : "Start transmission";
+            });
+
+            _resetButton.onClick.AddListener(() =>
+            {
+                _receivedSignalCount = 0;
+                _previousMeasuredSignalCount = 0;
+                _receivedSignalsPerSecond = 0;
+                _receivedSignalCount1 = 0;
+                _previousMeasuredSignalCount1 = 0;
+                _receivedSignalsPerSecond1 = 0;
+                _receivedSignalCount2 = 0;
+                _previousMeasuredSignalCount2 = 0;
+                _receivedSignalsPerSecond2 = 0;
+                _receivedSignalCount3 = 0;
+                _previousMeasuredSignalCount3 = 0;
+                _receivedSignalsPerSecond3 = 0;
+            });
+
+            _transport = new ENetTransport(useAnotherThread: true, targetFrameRate: 120, isBackground: true);
             _streamingClient = new SignalStreamingClient(_transport);
             _streamingClient.OnConnected += OnConnected;
             _streamingClient.OnDisconnected += OnDisconnected;
             _streamingClient.OnIncomingSignalDequeued += OnIncomingSignalDequeued;
-        }
-
-        async void Start()
-        {
-            Debug.Log($"[{nameof(SampleClient)}] StreamingClient is connecting...");
-            var connected = await _streamingClient.ConnectAsync(_connectParameters);
-            Debug.Log($"[{nameof(SampleClient)}] StreamingClient.IsConnected: {connected}");
-        }
-
-        void Update()
-        {
-            var sendOptions = new SendOptions(StreamingType.All, reliable: true);
-            _streamingClient.Send(messageId: 0, data: "Hello, world!", sendOptions);
         }
 
         void OnDestroy()
@@ -61,29 +125,144 @@ namespace SignalStreaming.Samples.ENetSample
             _transport.Dispose();
         }
 
+        async void Start()
+        {
+        }
+
+        void Update()
+        {
+            _transport.DequeueIncomingSignals();
+
+            _receivedSignalCountText.text = $"{_receivedSignalCount}";
+            _receivedSignalCountText1.text = $"{_receivedSignalCount1}";
+            _receivedSignalCountText2.text = $"{_receivedSignalCount2}";
+            _receivedSignalCountText3.text = $"{_receivedSignalCount3}";
+
+            var currentTimeMilliseconds = _stopwatch.ElapsedMilliseconds;
+            if (currentTimeMilliseconds - _previousMeasuredTimeMilliseconds > 1000)
+            {
+                var deltaTime = (currentTimeMilliseconds - _previousMeasuredTimeMilliseconds) / 1000f;
+                _receivedSignalsPerSecond = (_receivedSignalCount - _previousMeasuredSignalCount) / deltaTime;
+                _receivedSignalsPerSecond1 = (_receivedSignalCount1 - _previousMeasuredSignalCount1) / deltaTime;
+                _receivedSignalsPerSecond2 = (_receivedSignalCount2 - _previousMeasuredSignalCount2) / deltaTime;
+                _receivedSignalsPerSecond3 = (_receivedSignalCount3 - _previousMeasuredSignalCount3) / deltaTime;
+
+                _previousMeasuredTimeMilliseconds = currentTimeMilliseconds;
+                _previousMeasuredSignalCount = _receivedSignalCount;
+                _previousMeasuredSignalCount1 = _receivedSignalCount1;
+                _previousMeasuredSignalCount2 = _receivedSignalCount2;
+                _previousMeasuredSignalCount3 = _receivedSignalCount3;
+
+                _signalsPerSecondText.text = $"{_receivedSignalsPerSecond:F2} [signals/sec]";
+                _signalsPerSecondText1.text = $"{_receivedSignalsPerSecond1:F2} [signals/sec]";
+                _signalsPerSecondText2.text = $"{_receivedSignalsPerSecond2:F2} [signals/sec]";
+                _signalsPerSecondText3.text = $"{_receivedSignalsPerSecond3:F2} [signals/sec]";
+            }
+
+            if (_transmissionEnabled && _localPlayerMoveController != null)
+            {
+                var sendOptions = new SendOptions(StreamingType.All, reliable: true);
+
+                var position = _localPlayerMoveController.transform.position;
+                var rotation = _localPlayerMoveController.transform.rotation;
+
+                _streamingClient.Send((int)SignalType.PlayerObjectRotation, rotation, sendOptions);
+                _streamingClient.Send((int)SignalType.PlayerObjectPosition, position, sendOptions);
+            }
+        }
+
+        async void ConnectAsync(CancellationToken cancellationToken)
+        {
+            Debug.Log($"[{nameof(SampleClient)}] Trying to connect to server... (Thread: {Thread.CurrentThread.ManagedThreadId})");
+
+            var connected = false;
+            var joined = false;
+
+            var connectParameters = new ENetConnectParameters()
+            {
+                ConnectionRequestData = System.Text.Encoding.UTF8.GetBytes(_connectionKey),
+                ServerAddress = _serverAddress,
+                ServerPort = _port
+            };
+
+            connected = await _streamingClient.ConnectAsync(connectParameters, cancellationToken);
+            if (connected)
+            {
+                Debug.Log($"[{nameof(SampleClient)}] Connected to server. (Thread: {Thread.CurrentThread.ManagedThreadId})");
+                Debug.Log($"[{nameof(SampleClient)}] Trying to join group... (Thread: {Thread.CurrentThread.ManagedThreadId})");
+
+                joined = await _streamingClient.JoinGroupAsync(_groupId, cancellationToken);
+                if (joined)
+                {
+                    Debug.Log($"[{nameof(SampleClient)}] Joined group: {_groupId} (Thread: {Thread.CurrentThread.ManagedThreadId})");
+                }
+                else
+                {
+                    Debug.Log($"[{nameof(SampleClient)}] Failed to join group: {_groupId} (Thread: {Thread.CurrentThread.ManagedThreadId})");
+                }
+            }
+            else
+            {
+                Debug.Log($"[{nameof(SampleClient)}] Failed to connect. (Thread: {Thread.CurrentThread.ManagedThreadId})");
+            }
+
+            if (connected && joined)
+            {
+                _connectionButtonText.text = "Disconnect";
+            }
+            else
+            {
+                _connectionButtonText.text = "Connect to server";
+            }
+        }
+
         void OnConnected(uint clientId)
         {
             Debug.Log($"[{nameof(SampleClient)}] Connected - ClientId: {clientId}");
+            _clientId = clientId;
+            _playerMoveSystem.TryGetOrAdd(clientId, out _localPlayerMoveController);
+            _playerMoveSystem.UpdateColor(clientId, Color.cyan);
+            _playerMoveSystem.EnableAutopilot(clientId, true);
         }
 
         void OnDisconnected(string reason)
         {
             Debug.Log($"[{nameof(SampleClient)}] Disconnected - Reason: {reason}");
+            _playerMoveSystem.UpdateColor(_clientId, Color.red);
         }
 
         void OnIncomingSignalDequeued(int messageId, uint senderClientId, long originTimestamp, long transmitTimestamp, ReadOnlySequence<byte> payload)
         {
-            var originDateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(originTimestamp).ToString("MM/dd/yyyy hh:mm:ss.fff tt");
-            var transmitDateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(transmitTimestamp).ToString("MM/dd/yyyy hh:mm:ss.fff tt");
-
-            Debug.Log($"[{nameof(SampleClient)}] Received data sent from client[{senderClientId}]. " +
-                $"Message ID: {messageId}, Payload.Length: {payload.Length}, " +
-                $"OriginTimestamp: {originDateTimeOffset}, TransmitTimestamp: {transmitDateTimeOffset}");
+            _receivedSignalCount++;
 
             if (messageId == 0)
             {
                 var text = MessagePackSerializer.Deserialize<string>(payload);
-                Debug.Log($"<color=yello>[{nameof(SampleClient)}] Received message: {text}</color>");
+                // Debug.Log($"<color=yello>[{nameof(SampleClient)}] Received message: {text}</color>");
+            }
+            else if (messageId == (int)SignalType.PlayerObjectColor)
+            {
+                _receivedSignalCount1++;
+
+                if (senderClientId == _clientId) return;
+                var color = MessagePackSerializer.Deserialize<Color>(payload);
+                _playerMoveSystem.UpdateColor(senderClientId, color);
+            }
+            else if (messageId == (int)SignalType.PlayerObjectPosition)
+            {
+                _receivedSignalCount2++;
+
+                if (senderClientId == _clientId) return;
+                var position = MessagePackSerializer.Deserialize<Vector3>(payload);
+                _playerMoveSystem.UpdatePosition(senderClientId, position);
+            }
+            else if (messageId == (int)SignalType.PlayerObjectRotation)
+            {
+                _receivedSignalCount3++;
+
+                if (senderClientId == _clientId) return;
+                var rotation = MessagePackSerializer.Deserialize<Quaternion>(payload);
+                _playerMoveSystem.UpdateRotation(senderClientId, rotation);
             }
         }
     }
