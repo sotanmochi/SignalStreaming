e78e8a5694586de01a2c09c4e0fd8513f3120d9c
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-03-12 23:02:57 +0900

    Update SerializationTest

--- sandbox/Serialization/SerializationTest/Program.cs
+++ sandbox/Serialization/SerializationTest/Program.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using MessagePack;
 using MessagePack.Resolvers;
 using NetStack.Quantization;
+using NetStack.Serialization;
 using SignalStreaming.Serialization.MessagePack.NetStack;
 using UnityEngine;
 
@@ -16,6 +17,7 @@ namespace SignalStreaming.SerializationTest
         static void Main(string[] args)
         {
             QuantizedQuaternionTest();
+            QuantizedQuaternionTest2();
         }
 
         static void QuantizedQuaternionTest()
@@ -41,6 +43,59 @@ namespace SignalStreaming.SerializationTest
             Console.WriteLine("");
 
             var deserializedQq = MessagePackSerializer.Deserialize<QuantizedQuaternion>(serializedQq, options);
+            var dequantizedQ = SmallestThree.Dequantize(deserializedQq).ToUnityQuaternion();
+            var angle = Angle(q, dequantizedQ);
+
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] q: ({q.x}, {q.y}, {q.z}, {q.w})");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] dequantizedQ: ({dequantizedQ.x}, {dequantizedQ.y}, {dequantizedQ.z}, {dequantizedQ.w})");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Dot(q, dequantizedQ): {Dot(q, dequantizedQ)}");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Angle(q, dequantizedQ): {angle} [deg]");
+            Console.WriteLine("----------------------------------------");
+        }
+
+        static void QuantizedQuaternionTest2()
+        {
+            var q = GenerateRandomQuaternion();
+            var qq = SmallestThree.Quantize(q.ToSystemNumericsQuaternion());
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"        QuantizedQuaternionTest2");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Size of Quaternion: {Marshal.SizeOf(q)} bytes");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Size of QuantizedQuaternion: {Marshal.SizeOf(qq)} bytes");
+            Console.WriteLine("");
+
+            var bytesQ = new byte[1500];
+            var bytesSpanQ = new Span<byte>(bytesQ);
+
+            var bytesQq = new byte[1500];
+            var bytesSpanQq = new Span<byte>(bytesQq);
+
+            var bitButterQ = new BitBuffer(375); // ChunkCount: 375, BufferSize: 375 * 4 = 1500 bytes
+            var bitButterQq = new BitBuffer(375); // ChunkCount: 375, BufferSize: 375 * 4 = 1500 bytes
+
+            var serializedQLength = bitButterQ
+                                        .AddUInt(new UnionValue(q.x).AsUInt())
+                                        .AddUInt(new UnionValue(q.y).AsUInt())
+                                        .AddUInt(new UnionValue(q.z).AsUInt())
+                                        .AddUInt(new UnionValue(q.w).AsUInt())
+                                        .ToSpan(ref bytesSpanQ);
+            var serializedQqLength = bitButterQq
+                                        .AddUInt(qq.m)
+                                        .AddUInt(qq.a)
+                                        .AddUInt(qq.b)
+                                        .AddUInt(qq.c)
+                                        .ToSpan(ref bytesSpanQq);
+
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Serialized data size of Quaternion: {serializedQLength} bytes");
+            Console.WriteLine($"[{nameof(QuantizedQuaternionTest)}] Serialized data size of QuantizedQuaternion: {serializedQqLength} bytes");
+            Console.WriteLine("");
+
+            var bytesQqReadOnlySpan = new ReadOnlySpan<byte>(bytesQq);
+            var bitBuffer = new BitBuffer(375); // ChunkCount: 375, BufferSize: 375 * 4 = 1500 bytes
+            bitBuffer.FromSpan(ref bytesQqReadOnlySpan, serializedQqLength);
+
+            var deserializedQq = new QuantizedQuaternion(bitBuffer.ReadUInt(), bitBuffer.ReadUInt(), bitBuffer.ReadUInt(), bitBuffer.ReadUInt());
             var dequantizedQ = SmallestThree.Dequantize(deserializedQq).ToUnityQuaternion();
             var angle = Angle(q, dequantizedQ);
 
