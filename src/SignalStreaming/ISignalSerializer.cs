c6081dcfe3ee1b4101439b315ae52e8c15d7ea97
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-04-06 09:51:51 +0900

    feat: Update SignalStreaming.Serialization

--- src/SignalStreaming/ISignalSerializer.cs
+++ src/SignalStreaming/ISignalSerializer.cs
@@ -6,5 +6,6 @@ namespace SignalStreaming
     {
         void Serialize<T>(IBufferWriter<byte> writer, in T value);
         T Deserialize<T>(in ReadOnlySequence<byte> bytes);
+        void DeserializeTo<T>(T to, in ReadOnlySequence<byte> bytes) where T : class;
     }
 }
