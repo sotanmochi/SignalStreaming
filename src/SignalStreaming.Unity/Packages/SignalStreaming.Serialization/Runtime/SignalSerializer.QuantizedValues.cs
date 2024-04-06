81e14e7d5a994b1d61db03c4c322d70f43d8af96
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-04-06 09:51:51 +0900

    feat: Update SignalStreaming.Serialization

--- src/SignalStreaming.Unity/Packages/SignalStreaming.Serialization/Runtime/SignalSerializer.QuantizedValues.cs
+++ src/SignalStreaming.Unity/Packages/SignalStreaming.Serialization/Runtime/SignalSerializer.QuantizedValues.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Buffers;
-using NetStack.Quantization;
+using SignalStreaming.Quantization;
 using NetStack.Serialization;
 
 namespace SignalStreaming.Serialization
@@ -72,6 +73,173 @@ namespace SignalStreaming.Serialization
             var c = _bitBuffer.ReadUInt();
 
             return new QuantizedQuaternion(m, a, b, c);
+        }
+
+        void Serialize(IBufferWriter<byte> writer, QuantizedVector value)
+        {
+            if (value.Size > _bitBuffer.Length)
+            {
+                throw new ArgumentException($"QuantizedVector size {value.Size} is greater than the buffer size {_bitBuffer.Length}");
+            }
+
+            var span = writer.GetSpan();
+
+            _bitBuffer.Clear();
+            _bitBuffer.AddInt(value.Size);
+            for (var i = 0; i < value.Size; i++)
+            {
+                _bitBuffer.AddUInt(value.Elements[i]);
+            }
+
+            var length = _bitBuffer.ToSpan(ref span);
+            writer.Advance(length);
+        }
+
+        QuantizedVector DeserializeQuantizedVector(in ReadOnlySequence<byte> byteSequence)
+        {
+            var reader = new SequenceReader<byte>(byteSequence);
+            var span = reader.CurrentSpan;
+
+            _bitBuffer.Clear();
+            _bitBuffer.FromSpan(ref span, (int)byteSequence.Length);
+
+            // TODO: Multisegments
+
+            var size = _bitBuffer.ReadInt();
+            var quantizedVector = new QuantizedVector(size); // Allocation
+
+            for (var i = 0; i < size; i++)
+            {
+                quantizedVector.Elements[i] = _bitBuffer.ReadUInt();
+            }
+
+            return quantizedVector;
+        }
+
+        void DeserializeToQuantizedVector(in ReadOnlySequence<byte> byteSequence, QuantizedVector output)
+        {
+            var reader = new SequenceReader<byte>(byteSequence);
+            var span = reader.CurrentSpan;
+
+            _bitBuffer.Clear();
+            _bitBuffer.FromSpan(ref span, (int)byteSequence.Length);
+
+            // TODO: Multisegments
+
+            var size = _bitBuffer.ReadInt();
+            if (size != output.Size)
+            {
+                _bitBuffer.Clear();
+                throw new ArgumentException("Mismatched size");
+            }
+
+            for (var i = 0; i < size; i++)
+            {
+                output.Elements[i] = _bitBuffer.ReadUInt();
+            }
+        }
+
+        void Serialize(IBufferWriter<byte> writer, QuantizedHumanPose value)
+        {
+            _bitBuffer.Clear();
+
+            // BodyPosition
+            _bitBuffer.AddUInt(value.BodyPosition.x)
+                        .AddUInt(value.BodyPosition.y)
+                        .AddUInt(value.BodyPosition.z);
+
+            // BodyRotation
+            _bitBuffer.AddUInt(value.BodyRotation.m)
+                        .AddUInt(value.BodyRotation.a)
+                        .AddUInt(value.BodyRotation.b)
+                        .AddUInt(value.BodyRotation.c);
+
+            // Muscles
+            _bitBuffer.AddInt(value.Muscles.Size);
+            for (var i = 0; i < value.Muscles.Size; i++)
+            {
+                _bitBuffer.AddUInt(value.Muscles.Elements[i]);
+            }
+
+            var span = writer.GetSpan();
+            var length = _bitBuffer.ToSpan(ref span);
+            writer.Advance(length);
+        }
+
+        QuantizedHumanPose DeserializeQuantizedHumanPose(in ReadOnlySequence<byte> byteSequence)
+        {
+            var reader = new SequenceReader<byte>(byteSequence);
+            var span = reader.CurrentSpan;
+
+            _bitBuffer.Clear();
+            _bitBuffer.FromSpan(ref span, (int)byteSequence.Length);
+
+            // TODO: Multisegments
+
+            var x = _bitBuffer.ReadUInt();
+            var y = _bitBuffer.ReadUInt();
+            var z = _bitBuffer.ReadUInt();
+
+            var m = _bitBuffer.ReadUInt();
+            var a = _bitBuffer.ReadUInt();
+            var b = _bitBuffer.ReadUInt();
+            var c = _bitBuffer.ReadUInt();
+
+            var size = _bitBuffer.ReadInt();
+            if (size != QuantizedHumanPose.MuscleCount)
+            {
+                _bitBuffer.Clear();
+                throw new ArgumentException("Mismatched muscle count");
+            }
+
+            var quantizedHumanPose = new QuantizedHumanPose(); // Allocation
+            for (var i = 0; i < size; i++)
+            {
+                quantizedHumanPose.Muscles.Elements[i] = _bitBuffer.ReadUInt();
+            }
+            quantizedHumanPose.BodyPosition = new QuantizedVector3(x, y, z);
+            quantizedHumanPose.BodyRotation = new QuantizedQuaternion(m, a, b, c);
+
+            return quantizedHumanPose;
+        }
+
+        void DeserializeToQuantizedHumanPose(in ReadOnlySequence<byte> byteSequence, QuantizedHumanPose output)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var reader = new SequenceReader<byte>(byteSequence);
+            var span = reader.CurrentSpan;
+
+            _bitBuffer.Clear();
+            _bitBuffer.FromSpan(ref span, (int)byteSequence.Length);
+
+            // TODO: Multisegments
+
+            var x = _bitBuffer.ReadUInt();
+            var y = _bitBuffer.ReadUInt();
+            var z = _bitBuffer.ReadUInt();
+
+            var m = _bitBuffer.ReadUInt();
+            var a = _bitBuffer.ReadUInt();
+            var b = _bitBuffer.ReadUInt();
+            var c = _bitBuffer.ReadUInt();
+
+            var size = _bitBuffer.ReadInt();
+            if (size != output.Muscles.Size)
+            {
+                _bitBuffer.Clear();
+                throw new ArgumentException("Mismatched muscle count");
+            }
+
+            for (var i = 0; i < size; i++)
+            {
+                output.Muscles.Elements[i] = _bitBuffer.ReadUInt();
+            }
+            output.BodyPosition = new QuantizedVector3(x, y, z);
+            output.BodyRotation = new QuantizedQuaternion(m, a, b, c);
         }
     }
 }
--- src/SignalStreaming.Unity/Packages/SignalStreaming.Serialization/Runtime/SignalSerializer.cs
+++ src/SignalStreaming.Unity/Packages/SignalStreaming.Serialization/Runtime/SignalSerializer.cs
@@ -1,6 +1,6 @@
 using System.Buffers;
 using MessagePack;
-using NetStack.Quantization;
+using SignalStreaming.Quantization;
 
 namespace SignalStreaming.Serialization
 {
@@ -23,6 +23,14 @@ namespace SignalStreaming.Serialization
             {
                 Serialize(writer, (QuantizedQuaternion)(object)value);
             }
+            else if (typeof(T) == typeof(QuantizedVector))
+            {
+                Serialize(writer, (QuantizedVector)(object)value);
+            }
+            else if (typeof(T) == typeof(QuantizedHumanPose))
+            {
+                Serialize(writer, (QuantizedHumanPose)(object)value);
+            }
             else
             {
                 MessagePackSerializer.Serialize(writer, value, _serializerOptions);
@@ -39,9 +47,33 @@ namespace SignalStreaming.Serialization
             {
                 return (T)(object)DeserializeQuantizedQuaternion(bytes);
             }
+            else if (typeof(T) == typeof(QuantizedVector))
+            {
+                return (T)(object)DeserializeQuantizedVector(bytes);
+            }
+            else if (typeof(T) == typeof(QuantizedHumanPose))
+            {
+                return (T)(object)DeserializeQuantizedHumanPose(bytes);
+            }
             else
             {
                 return MessagePackSerializer.Deserialize<T>(bytes, _serializerOptions);
+            }
+        }
+
+        public void DeserializeTo<T>(T to, in ReadOnlySequence<byte> bytes) where T : class
+        {
+            if (typeof(T) == typeof(QuantizedVector))
+            {
+                DeserializeToQuantizedVector(bytes, (QuantizedVector)(object)to);
+            }
+            else if (typeof(T) == typeof(QuantizedHumanPose))
+            {
+                DeserializeToQuantizedHumanPose(bytes, (QuantizedHumanPose)(object)to);
+            }
+            else
+            {
+                throw new System.ArgumentException($"Type {typeof(T)} is not supported");
             }
         }
     }
