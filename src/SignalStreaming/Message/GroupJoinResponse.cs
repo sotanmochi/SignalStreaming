4843926cbf9c61fc52d8033c2e75b213388ec5ed
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-05-06 14:01:46 +0900

    Delete unnecessary code

--- src/SignalStreaming/Message/GroupJoinResponse.cs
+++ src/SignalStreaming/Message/GroupJoinResponse.cs
@@ -1,16 +1,9 @@
-using System;
-using MessagePack;
-
 namespace SignalStreaming
 {
-    [MessagePackObject]
     public readonly struct GroupJoinResponse
     {        
-        [Key(0)]
         public readonly bool RequestApproved;
-        [Key(1)]
         public readonly string GroupId;
-        [Key(2)]
         public readonly string ConnectionId;
 
         public GroupJoinResponse(bool requestApproved, string groupId, string connectionId)
--- src/SignalStreaming/Message/GroupLeaveRequest.cs
+++ src/SignalStreaming/Message/GroupLeaveRequest.cs
@@ -1,14 +1,8 @@
-using System;
-using MessagePack;
-
 namespace SignalStreaming
 {
-    [MessagePackObject]
     public readonly struct GroupLeaveRequest
     {
-        [Key(0)]
         public readonly string GroupId;
-        [Key(1)]
         public readonly string ConnectionId;
 
         public GroupLeaveRequest(string groupId, string connectionId)
