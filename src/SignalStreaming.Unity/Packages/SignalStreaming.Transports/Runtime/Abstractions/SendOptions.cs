98d51aa56f138ac227054073e1f6ebd945cb8c5e
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-05-18 15:57:14 +0900

    Add SignalStreaming.Transports.Abstractions

---
+++ src/SignalStreaming.Unity/Packages/SignalStreaming.Transports/Runtime/Abstractions/SendOptions.cs
@@ -0,0 +1,25 @@
+namespace SignalStreaming.Transports
+{
+    public struct SendOptions
+    {
+        public readonly StreamingType StreamingType;
+        public readonly bool Reliable;
+
+        public SendOptions(StreamingType streamingType, bool reliable)
+        {
+            StreamingType = streamingType;
+            Reliable = reliable;
+        }
+    }
+
+    public enum StreamingType : byte
+    {
+        ToHubServer = 0,
+        ToOneClient = 1,
+        ToManyClients = 2,
+        ExceptSelf = 3,
+        ExceptOne = 4,
+        ExceptMany = 5,
+        All = 6,
+    }
+}
