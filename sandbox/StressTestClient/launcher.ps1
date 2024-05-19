1f9ad8fa0d071939f6b1b198a2061a133d2f8c23
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-05-19 21:35:27 +0900

    Update stress test client launcher

--- sandbox/StressTestClient/launcher.ps1
+++ sandbox/StressTestClient/launcher.ps1
@@ -1,13 +1,43 @@
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 param (
+    [string]$exePath,
     [int]$processCount,
-    [string]$exePath
+    [string]$server,
+    [int]$port
 )
 
-for ($i = 1; $i -le $processCount; $i++) {
-    Start-Process -FilePath $exePath
+Write-Output "----------------------------------------"
+Write-Output "StressTestClient Launcher"
+Write-Output "----------------------------------------"
+
+$arguments = @()
+
+if ($server) {
+    $arguments += "--server $server"
 }
 
-Write-Output "Start $exePath"
-Write-Output "Process count: $processCount"
+if ($port) {
+    $arguments += "--port $port"
+}
+
+Write-Output "Exe: $exePath"
+Write-Output "Parameters: $($arguments -join ' ')"
+Write-Output ""
+Write-Output "Toral process count: $processCount"
+Write-Output ""
+
+for ($i = 1; $i -le $processCount; $i++) {
+    if ($arguments.Count -gt 0) {
+        Start-Process -FilePath $exePath -ArgumentList $arguments
+    } else {
+        Start-Process -FilePath $exePath
+    }
+
+    Write-Output "Start process[$i]"
+    Start-Sleep -Seconds 1
+}
+
+Write-Output ""
+Write-Output "Toral process count: $processCount"
+Write-Output "----------------------------------------"
