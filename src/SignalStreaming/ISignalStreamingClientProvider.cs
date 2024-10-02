24701478fd5c44832570a7e1946664a8e04a84d8
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2024-10-02 09:07:22 +0900

    Update provider interface

--- src/SignalStreaming/ISignalStreamingClientProvider.cs
+++ src/SignalStreaming/ISignalStreamingClientProvider.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace SignalStreaming
 {
     public interface ISignalStreamingClientProvider
     {
+        event Action OnClientAdded;
+        event Action OnAliasAdded;
+        event Action OnAliasUpdated;
+        ISignalStreamingClient GetOrAdd(string serverUrl);
+        ISignalStreamingClient GetFirstOrDefault();
+        void AddOrUpdateAlias(string alias, string serverUrl);
         bool TryGet(string serverUrl, out ISignalStreamingClient client);
+        bool TryGetByAlias(string alias, out ISignalStreamingClient client);
     }
 }
